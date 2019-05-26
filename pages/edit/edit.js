79ecd3426f1d60063ed9c6e9488c07fb4e3619fa
yl0416 <48374010+yl0416@users.noreply.github.com>
2019-05-26 13:25:16 +0800

    Update edit.js

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -5,6 +5,9 @@ Page({
      * 页面的初始数据
      */
     data: {
+        result:'',
+    state:''
+
 
     },
 
@@ -12,6 +15,8 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        console.log('App onLoad');
+
 
     },
 
@@ -19,13 +24,16 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+        console.log('App onReady');
+  
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
+        console.log('App onShow');
+  
 
     },
 
@@ -33,6 +41,8 @@ Page({
      * 生命周期函数--监听页面隐藏
      */
     onHide: function () {
+        console.log('App onHide');
+  
 
     },
 
@@ -40,6 +50,7 @@ Page({
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
+        console.log('App onUnload');
 
     },
 
@@ -47,6 +58,7 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
+        console.log('下拉动作');
 
     },
 
@@ -54,13 +66,25 @@ Page({
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
+        console.log('到底了');
 
     },
+    
+    /**
+     * 页面滚动
+     */
+    onPageScroll:function(options){
+        console.log('options');
+    }
 
     /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
-
+        console.log('用户点击右上角分享');
+        return{
+            title:'tst页面',
+            path:'/page/tst?id=123'
+        }
     }
 })
